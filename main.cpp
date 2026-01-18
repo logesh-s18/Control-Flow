d4da055d40c38ee18d0968d0ff300738a4d0e68c
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-01-18 14:00:18 +0530

    8.2 - correct way of using if/else structure - always use braces for better readability even if it is single line of statment for that condition

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 namespace sports::longJump
 {
-	constexpr int maxHeightAllowed {154};
+	constexpr int maxHeightAllowed{ 154 };
 }
 
 
@@ -13,9 +13,9 @@ int main()
 {
 
 
-	cout << "Hi there, welcome to Miami Atheloo Day!!!\n\n" 
-		    
-			"Please enter your height here (in cm) : ";
+	cout << "Hi there, welcome to Miami Atheloo Day!!!\n\n"
+
+		"Please enter your height here (in cm) : ";
 
 	int playerHeight;
 
@@ -23,27 +23,34 @@ int main()
 
 	cout << "\n\nYour Height " << playerHeight << " is ";
 
-	
-	
+
+
 	namespace spLngJmp = sports::longJump;  //namespace usuage type 3
 
 	using namespace spLngJmp;   //namespace usuage type 3
 
-	
-	
+
+
 	//playerHeight validation
 
 	int x{};
 
 	if (playerHeight <= maxHeightAllowed)
+	{
 		if (playerHeight == 111)
+		{
 			x = 7777;
-
-		else //dangling else. this is a pair of innermost if condition and not with outer. specify clearly with braces '{ }' if u need to mention it for outer
+		}
+		else
+		{
 			x = 9999;
 
+		}
+	}
 	else
+	{
 		x = 8888;
 
 		std::cout << x;
+	}
 }
