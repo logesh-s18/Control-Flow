4b50ab529a39359fa801a419e3bea5ed3849954d
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-02-14 22:30:51 +0530

    8.8 - Quiz 2 - learncpp answer

--- main.cpp
+++ main.cpp
@@ -21,27 +21,16 @@ using namespace std;
 
 int main() 
 {
-	//giving the column heading for terminal before the loop
 
-	char a = 'a';
+	//simple, no need to create a variable for 'z'
 
-	char z = 'z';
-
-
-	//using 'a' as looping variable where the loop start and end depends on.
-
-	while (a <= z) // here the comparision will happen based on their ASCII. so the iteration until it reaches 'z' ASCII, loop condition will be true.
+	char myChar{ 'a' };
+	while (myChar <= 'z')
 	{
-		//im printing the alphabets and casting the char's int value to explicitly print the ASCII values too. bcz, char wont print ASCII, it will print the original character value
-		cout << a << " - " << static_cast<int>(a) << '\n';
-		
-
-
-		++a; // incrementing the ASCII which goes for next alphabet's ASCII
-
-
-
+		std::cout << myChar << ' ' << static_cast<int>(myChar) << '\n';
+		++myChar;
 	}
 
 	return 0;
 }
+
