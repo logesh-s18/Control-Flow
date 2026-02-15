03f04f43a0dee2bd107d360cc79e571adcadc08f
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-02-15 16:49:42 +0530

    8.8 - iteration count check

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 
 
-/*
-https://www.learncpp.com/cpp-tutorial/introduction-to-loops-and-while-statements/
-
-Question #4
-
-Now make the numbers print like this:
-
-		1
-	  2 1
-	3 2 1
-  4 3 2 1
-5 4 3 2 1
-
-*/
-
 #include <iostream>
 
 using namespace std;
@@ -22,45 +7,34 @@ using namespace std;
 
 int main()
 {
-
-	int rows{ 1 };
-
-	while (rows <= 5)
+	int i = 1;
+	int count = 0;
+	while (i < 3)
 	{
+		int j = 1;
 
-		int cols{ 5 }; // explicitly giving bcz we know that it needs 5 columns and also number need to be printed when rows is same or less while decrementing from 5
-
-		while (cols > 0) // which means, to make this false, we need to dec the loop variable
+		while (j < 3)
 		{
+			int k = 1;
 
-			// for eg: if row 2 means, while iterating to scan nums and to print, print the number only when the col iteration is == or less than the rows as we decrementing from 5 to 1
-			if (cols <= rows) 
-				cout << cols << ' ';
-			else //for any number which is greater than row, than we print space
-				cout << " " << ' ';
+			while (k < 3)
+			{
+				cout << "HI\n";
+				++count;
+				++k;
+			}
+			++count;
 
-			--cols;  // decrementing to scan from 5 to 1 for each row iteration
-
+			++j;
 		}
 
-		cout << '\n';
+		++count;
+		++i;
 
-		++rows;
 	}
+
+	cout << "total : " << count;
 
 	return 0;
 }
 
-/*
-
-
-Output:
-
-		1
-	  2 1
-	3 2 1
-  4 3 2 1
-5 4 3 2 1
-
-
-*/
