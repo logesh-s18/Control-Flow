3caf70fd8480d7dbfcdf551a9b2dafe705b7eee9
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-02-24 17:38:27 +0530

    8.10 — For statements - wrong altering for loop 1

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main() {
 	
 	
 	int i = 0;
-	for (  ; i < 5; i++ )
+
+	bool isTrue = i < 5;
+
+	for (  ; isTrue; )
 	{
 		std::cout << "Please enter a number between 1 and 5: \n";
-		
+		i++;
 	}
 
 	std::cout << "you have entered: " << selection << std::endl;
