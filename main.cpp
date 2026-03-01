fbce99b1e9e5acd895a82e2d7bb2147abf8ef22d
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-03-02 00:09:33 +0530

    8.10 — For loop - right side loop variable dominates the left side loop variable

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-    for (int x{ 0 }, y{ 9 }; x < 10, y >= 0; ++x, --y)
+	for (int x{ 0 }, y{ 9 }; y <= 0, x < 10; ++x, --y) //right side loop variable dominates the left side loop variable
         std::cout << x << ' ' << y << '\n';
 
     return 0;
