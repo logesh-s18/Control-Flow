36360ab437fcb28b40e5580b6d6b275e357f8de3
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-03-02 02:37:01 +0530

    8.10 — For loop - Omitted expressions

--- main.cpp
+++ main.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
 
 
-std::int64_t pow(int base, int exponent)
-{
-    std::int64_t total{ 1 };
-
-    for (int i{ 0 }; i < exponent; ++i) // init will only run one time
-    {
-        total *= base;
-    }
-
-
-    return total;
-}
 
 int main() {
 
-    for (int i{ 0 }; i < 10; ++i) // uses <, still terminates
+    int i{ 0 };
+    for (; i < 10; ) // no init-statement or end-expression
     {
-        std::cout << i;
-        if (i == 9) ++i; // jump over value 10
+        std::cout << i << ' ';
+        ++i;
     }
 
-    for (int i{ 0 }; i != 10; i++) // uses !=, infinite loop
-    {
-        std::cout << i;
-        if (i == 9) ++i; // jump over value 10
-    }
+    std::cout << '\n';
+
+    return 0;
 
 }
