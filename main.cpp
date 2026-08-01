203df5743b02f61a9cacd16282e118d82bfba128
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-08-01 21:23:47 +0530

    8.x — Chapter 8 summary and quiz - Q1

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ namespace Constant
 }
 
 
+
 double getHeightOfTwr()
 {
 	std::cout << "Enter the height of the tower in meters: ";
@@ -30,37 +31,36 @@ double getHeightOfTwr()
 
 void printBallHeight(double currentBallHeight, int sec)
 {
-	
-
 	// -ve resembles how deep inside into ground the ball went. we dont need that, just say its in ground
 	if (currentBallHeight > 0)
-		std::cout << "At " << sec << " second/s, the ball is at height: " << currentBallHeight << " meters \n";
+		std::cout << "At " << sec << " seconds, the ball is at height: " << currentBallHeight << " meters \n";
 	else // if currentBallHeight goes negative 
-		std::cout << "At " << sec << " second/s, the ball is on Ground \n";
-
-
+		std::cout << "At " << sec << " seconds, the ball is on Ground \n";
 }
 
 
 
 
-double getCurrentBallHeight(double TwrHeight, int sec)
+double getCurrentBallHeight(double TwrHeight, int seconds)
 {
-	//formula: distance fallen = gravity_constant * x_seconds2 / 2
-	double distanceFallen = (Constant::gravity * (sec * sec)) / 2.0;
 
-	double currentBallHeight = TwrHeight - distanceFallen;
 
-	// ground hit logic
-	if (currentBallHeight < 0.0)
-		return 0.0;
+		//formula: distance fallen = gravity_constant * x_seconds2 / 2
+		double distanceFallen = (Constant::gravity * (seconds * seconds)) / 2.0;
+
+		double currentBallHeight = TwrHeight - distanceFallen;
+
+		if (currentBallHeight < 0.0)
+			return 0.0;
 
 	//if not ground, no above return
 	return currentBallHeight;
 
 }
 
-void calcAndPrint(double TwrHeight, int sec)
+
+
+double calcAndPrint(double TwrHeight, int sec)
 {
 
 	// we need to collect current ball height after every secs it falls down, to find balance distance it needs to reach ground
@@ -69,19 +69,26 @@ void calcAndPrint(double TwrHeight, int sec)
 	// final output print msg
 	printBallHeight(currentBallHeight, sec);
 
+	//After priting msg, return the current height to loop
+	return currentBallHeight;
+
 }
+
 
 int main()
 {
 	//get the height from user
 	double heightOfTwr{ getHeightOfTwr() };
 
-	calcAndPrint(heightOfTwr, 0);
-	calcAndPrint(heightOfTwr, 1);
-	calcAndPrint(heightOfTwr, 2);
-	calcAndPrint(heightOfTwr, 3);
-	calcAndPrint(heightOfTwr, 4);
-	calcAndPrint(heightOfTwr, 5);
+	double currentHeightofTwr{heightOfTwr};
+
+
+	// condition = if current height returned from loop is still not 0, then inc the secs, 
+	// if the current height is 0, then stop the loop
+	for (int sec = 0; currentHeightofTwr > 0; ++sec)
+	{
+		currentHeightofTwr = calcAndPrint(heightOfTwr, sec);
+	}
 
 	return 0;
 
