ac3cd0f1b7efaaed56a96aa0092ac749642fa083
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-08-02 19:35:29 +0530

    8.x — Chapter 8 summary and quiz - Q2 cracked !!!

--- main.cpp
+++ main.cpp
@@ -1,61 +1,62 @@
+// Make sure that assert triggers even if we compile in release mode
+#define NDEBUG
+
+#undef NDEBUG
+#include <cassert> // for assert
 #include <iostream>
 
-// Simulates a very fast function
-// Worker function (leaf function)
-void fastFunction()
+bool isPrime(int x)
 {
-    volatile long long sum{};
 
-    for (int i = 0; i < 5'000'000; ++i)
-    {
-        sum += i;
-    }
+	//as per the Prime logic, 0 & 1 are not in scope for Prime, since 0 will be undefined and 1 will be always divisible. 1 is considered to be unit number
+
+	/*--- Perfect Guard Clause ---*/
+	if (x <= 1)
+		return false;
+
+
+	// this loop is to check if numbers in - between 2 to 'x' got divisible
+	// which prooves there will be another number which can divide the x than itself 
+	
+	/*---- Flawless Imposter Catching  ----*/
+	for (int i = 2; i < x; ++i)
+	{
+
+		if ((x % i) == 0)
+		{
+			return false;
+		}
+	}
+
+	//there is no divisible number gets true (i<x), then the next inc would be the number itself which is divisible, so we make it fals 
+	// and skip loop, and return 'true' if no in-between nums found. its a PRIME
+
+	/*--- 3. The Survivor ---*/
+	return true;
+
 }
 
-// Simulates a medium-speed function
-// Worker function (leaf function)
-void mediumFunction()
+int main()
 {
-    volatile long long sum{};
+	assert(!isPrime(0)); 
+	assert(!isPrime(1));
+	assert(isPrime(2)); 
+	assert(isPrime(3));
+	assert(!isPrime(4));
+	assert(isPrime(5));
+	assert(isPrime(7));
+	assert(!isPrime(9));
+	assert(isPrime(11));
+	assert(isPrime(13));
+	assert(!isPrime(15));
+	assert(!isPrime(16));
+	assert(isPrime(17));
+	assert(isPrime(19));
+	assert(isPrime(97));
+	assert(!isPrime(99));
+	assert(isPrime(13417));
 
-    for (int i = 0; i < 50'000'000; ++i)
-    {
-        sum += i;
-    }
-}
+	std::cout << "Success!\n";
 
-// Simulates a slow function
-// Worker function (leaf function)
-void slowFunction()
-{
-    volatile long long sum{};
-
-    for (int i = 0; i < 200'000'000; ++i)
-    {
-        sum += i;
-    }
-}
-
-// Calls all three
-// Orchestrator Function (Caller)
-// Delegates work to helper/worker functions.
-void processData()
-{
-    fastFunction();     // child; leaf function
-    mediumFunction();   // child; leaf function
-    slowFunction();     // child; leaf function
-}
-
-
-
-
-int main() // program entry point; caller; parent function
-{
-    std::cout << "Program Started\n\n";
-
-    processData(); //caller; orchestrator;
-
-    std::cout << "\nProgram Finished\n";
-
-    return 0;
+	return 0;
 }
